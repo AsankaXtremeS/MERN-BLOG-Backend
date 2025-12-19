b183d692151fe4d7c3a77e493b624917d927ea1f
asanka <asankasampath200228@gmail.com>
2025-12-19 11:39:45 +0530

    feat: enhance user registration to include user creation and response handling

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,7 @@
-const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
 
+const User = require('../models/userModel');
+const HttpError = require('../models/errorModel');
 
 
 
@@ -29,7 +31,20 @@ const registerUser = async (req, res, next) => {
             return next(new HttpError('Passwords do not match.', 422));
         }
 
-        
+        const salt =  await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            name,
+            email: newEmail,
+            password: hashedPassword
+        })
+        res.status(201).json(newUser);
+
+
+
+        await newUser.save();
+        res.status(201).json({message: 'User registered successfully'});
 
     } catch (error) {
         return next(new HttpError('Registering user failed, please try again later.', 422));
